c87c9bdaf68f2543697370009d13a9461fa40572
Dave Bevan <dave.bevan@bbc.co.uk>
2020-06-17 11:10:54 +0100

    refine whoami fn

--- bbc-newslabs-header.js
+++ bbc-newslabs-header.js
@@ -123,12 +123,18 @@ id="rect839" width="209.86325" height="221.2072" x="335.46667" y="-34.743092" />
                 this.userinfo=user.displayname + '\n' + user.department + '\n' + user.mail
                 window.bbc=window.bbc||{}
                 bbc.userinfo=user
-                bbc.whois=function(id)
+                bbc.whoami=function(onResponseCB)
                 {
-                    fetch('/generic-apis/whois/'+id)
+                    if (typeof(onResponseCB)!=='function') {
+                        console.log('Usage: bbc.whoami(fn)')
+                        return
+                    }
+                    fetch('/generic-apis/whois/'+bbc.userinfo.userid)
                     .then(this.fetchError)
                     .then(resp=>resp.json())
-                    .then(json=>json)
+                    .then(json=>{
+                        onResponseCB(json)
+                    })
                 }
                 bbc.onReith=function(onReithCB, offReithCB)
                 {
