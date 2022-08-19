743fc3d03e895abf8ae7003a751fbea71df94a30
Dave Bevan <dave.bevan@bbc.co.uk>
2022-08-19 12:59:53 +0100

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -1,10 +1,14 @@
 (function () {
 
   const loadComponent = (component) => {
+    const n = document.querySelector('head script[src*="newslabs-elements/autoload.js"]')
     const s = document.createElement('script')
     s.src = `https://bbc.github.io/newslabs-elements/${component}.js`
-    console.log(`loadComponent: ${s.src}`)
-    const n = document.querySelector('head script[src*="newslabs-elements/autoload.js"]')
+    s.setAttribute('component', component)
+    if (n.src.indexOf('debug') > -1) {
+      s.setAttribute('debug', true)
+      console.log(`loadComponent: ${s.src}`)
+    }
     n.after(s)
   }
   
