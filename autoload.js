e25029c6444b3cf6bc0283424f66516c56c310a6
Dave Bevan <dave.bevan@bbc.co.uk>
2022-08-19 11:59:10 +0100

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -1,13 +1,14 @@
+(function () {
 
-window.BBC = window.BBC || {}
-window.BBC.NE = window.BBC.NE || {}
+  const loadComponent = (component) => {
+    const s = document.createElement('script')
+    s.src = `https://bbc.github.io/newslabs-elements/${component}.js`
+    console.log(`loadComponent: ${s.src}`)
+    const n = document.querySelector('head script[src*="newslabs-elements/autoload.js"]')
+    n.after(s)
+  }
+  
+  loadComponent('bbc-newslabs-header')
+  loadComponent('bbc-newslabs-footer')
 
-BBC.NE.loadComponent = (component) => {
-  const s = document.createElement('script')
-  s.src = `https://bbc.github.io/newslabs-elements/${component}.js`
-  const n = document.querySelector('head script[src*="newslabs-elements/autoload.js"]')
-  n.after(s)
-}
-
-BBC.NE.loadComponent('bbc-newslabs-header')
-BBC.NE.loadComponent('bbc-newslabs-footer')
+})()
