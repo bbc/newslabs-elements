43434627e5339303e52700f8f600dc45786321de
Dave Bevan <dave.bevan@bbc.co.uk>
2022-08-19 11:42:40 +0100

    Create autoload.js

---
+++ autoload.js
@@ -0,0 +1,10 @@
+
+loadComponent = (component) => {
+  const s = document.createElement('script')
+  s.src = `https://bbc.github.io/newslabs-elements/${component}.js`
+  const n = document.querySelector('head script[src*="newslabs-elements/autoload.js"]')
+  n.after(s)
+}
+
+loadComponent('bbc-newslabs-header')
+loadComponent('bbc-newslabs-footer')
