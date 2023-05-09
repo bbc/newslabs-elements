31f4e3dd0d32497a3dd9d826352250924f54c227
Dave Bevan <dave.bevan@bbc.co.uk>
2023-05-09 17:56:05 +0100

    Update bbc-newslabs-header.js

--- bbc-newslabs-header.js
+++ bbc-newslabs-header.js
@@ -63,6 +63,7 @@
             date = value; // assume a date object has been passed
         }
         const seconds = Math.min(1, Math.floor((Date.now() - date.getTime()) / 1000));
+        console.log(`bbc.timeSince(${value}) seconds=${seconds}`);
         const interval = intervals.find(i => i.seconds < seconds);
         const count = Math.floor(seconds / interval.seconds);
         return `${count} ${interval.label}${count !== 1 ? 's' : ''} ago`;
